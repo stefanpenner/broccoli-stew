dbe69a2679ed7ffe87bfb54ce931800586010afe
Taras Mankovski <tarasm@gmail.com>
2015-03-27 06:20:35 -0400

    npm.entry tests are passing

--- tests/npm-test.js
+++ tests/npm-test.js
@@ -4,10 +4,17 @@ var expect = chai.expect;
 var helpers = require('broccoli-test-helpers');
 var makeTestHelper = helpers.makeTestHelper;
 var cleanupBuilders = helpers.cleanupBuilders;
-var _npm = require('../lib/npm');
+var rewire =  require('rewire');
 
 describe('npm', function() {
   var fixturePath = path.join(__dirname, 'fixtures', 'npm');
+
+  function mockResolve(moduleName) {
+    return path.join(fixturePath, 'node_modules', moduleName);
+  }
+
+  var _npm = rewire('../lib/npm');
+  _npm.__set__('resolve', mockResolve);
 
   afterEach(function() {
     return cleanupBuilders();
@@ -21,29 +28,38 @@ describe('npm', function() {
     }
   });
 
-  it("return tree with file in entry", function() {
-    return entry('bar').then(function(result){
-      var files = result.files;
+  describe('entry', function(){
+    it("returns tree with file from main", function() {
+      return entry('bar').then(function(result){
+        var files = result.files;
 
-      expect(files).to.eq([
-        'stool.js'
-      ]);
+        expect(files).to.eql([
+          // TODO: ask stef why find returns relative paths
+          path.join(mockResolve('bar'), 'stool.js').substring(1)
+        ]);
+      });
+    });
+
+    it("renames file in entry when new file name given", function(){
+      return entry('bar', 'hot/chili.js').then(function(result){
+        var files = result.files;
+
+        expect(files).to.eql([
+          'hot/chili.js'
+        ]);
+      })
+    });
+
+    it("entry throws an exception when not present in package.json", function(){
+      return entry('baz').catch(function(error){
+        expect(error).to.eq('package.json for baz does not have main property');
+      });
     });
   });
 
-  it("renames file in entry when new file name given", function(){
-    return entry('bar', 'hot/chili.js').then(function(result){
-      var files = result.files;
-
-      expect(files).to.eq([
-        'hot/chili.js'
-      ])
-    })
-  });
-
-  it("entry throws an exception when not present in package.json", function(){
-    return entry('baz').catch(function(error){
-      expect(error.message).to.eq('package.json for baz does not have main property');
+  describe('entryPath', function(){
+    it('return path to script in package.json#main', function(){
+      expect(_npm.entryPath('bar')).to.eq(path.join(mockResolve('bar'), 'stool.js'));
     });
   });
 });
